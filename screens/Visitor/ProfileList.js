df2ccba7b7767ad6cf10b1870c67d107e2525d66
jongoh-lee <sky40165285@gmail.com>
2021-06-10 03:15:30 +0900

    PROFILE LIST TO FLATLIST

--- screens/Visitor/ProfileList.js
+++ screens/Visitor/ProfileList.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { StyleSheet, View, Text, RefreshControl } from "react-native";
 import DangolCard from "../../components/Visitor/DangolCard";
-import { ScrollView } from "react-native-gesture-handler";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
 import { useQuery } from "@apollo/react-hooks";
 import { SEARCH_PROFILE } from "./VisitorQueries";
 import ScreenLoader from "../../components/Custom/ScreenLoader";
+import Caption from "../../components/Custom/Caption";
 
 const styles = StyleSheet.create({
   container:{
@@ -21,7 +22,7 @@ export default () => {
             name: ""
         }
   });
-  
+
   const refresh = async () => {
     try {
       setRefreshing(true);
@@ -33,16 +34,29 @@ export default () => {
     }
   }
 
+  const emptyComponent = () => (
+    <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+        <Caption>목록이 없습니다</Caption>
+    </View>
+  )
+
+  const renderItem = ({item}) => {
+    return(
+      <DangolCard {...item}/>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      {loading? <ScreenLoader/> : null}
-        <ScrollView 
-          showsVerticalScrollIndicator={false} 
-          contentContainerStyle={{padding:10, paddingTop:0, flexGrow:1}} 
-          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refresh} />}>
-          {data?.searchProfile && (
-            data?.searchProfile.map( (data, index) => <DangolCard key={index} {...data}/>)
-          )}
-        </ScrollView>
+      {loading? <ScreenLoader/> : 
+       <FlatList
+       data={data?.searchProfile.sort((a, b) => b.dangolCount - a.dangolCount)}
+       renderItem={renderItem}
+       keyExtractor={item => item.id}
+       showsVerticalScrollIndicator={false}
+       contentContainerStyle={{padding:10, paddingTop:0, flexGrow:1}}
+       refreshControl={<RefreshControl refreshing={refreshing} onRefresh={refresh} />}
+       ListEmptyComponent={emptyComponent}
+      />}
   </View>
 )};
